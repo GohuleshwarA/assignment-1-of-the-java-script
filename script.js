aaa3de1f298ebf53ce661b527b55d7e544632a65
GohuleshwarA <99191340+GohuleshwarA@users.noreply.github.com>
2022-03-09 23:16:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+var str = "session 1 of java script"
+console.log(str)
+
+var num= 10-3
+console.log("This is Number "+ num)
+
+var float=1.5+2.3
+console.log("This is float " + float)
+
+var boolean=7>6
+console.log(boolean)
+
+var boolean=5>6
+
+console.log(boolean)
+
+// Area of circle
+
+let two = 2
+const pi = 3.14
+
+var radius = 5
+console.log("Area of circle = " + pi*(radius*radius))
+
+//Circumference of circle
+
+var radius= 5
+console.log("Circumference of circle = "+ two*pi*radius)
+
+
+//Radius of the circle
+
+var diameter= 12
+console.log("Radius of the circle = " + diameter/two)
+
